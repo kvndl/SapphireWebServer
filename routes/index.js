0bf402d704af80d9e311fba7535874e17664a0d7
Noah Schepers <admin@noahschepers>
2020-05-09 00:38:14 -0400

    editing title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'NPS', message: 'Wow, it works!' });
+  res.render('index', { title: 'NPS', message: 'Wow, it works! Again!' });
 });
 
 module.exports = router;
