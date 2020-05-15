4f1b740daecdbaf52b6faa876163a85ffd467614
Noah Schepers <admin@noahschepers>
2020-05-15 13:24:23 -0400

    moving routes to the main indexer

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,28 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'NPS' });
 });
 
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'ABOUT PAGE' });
+});
+
+/* GET code page. */
+router.get('/code', function(req, res, next) {
+  res.render('code', { title: 'CODE PAGE' });
+});
+
+router.get('/media', function(req, res, next) {
+  res.render('media', { title: 'MEDIA PAGE' });
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'CONTACT PAGE' });
+});
+
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'LOGIN PAGE' });
+});
+
 module.exports = router;
--- routes/media.js
+++ routes/media.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET media page. */
 router.get('/', function(req, res, next) {
   res.render('media', { title: 'MEDIA PAGE' });
 });
